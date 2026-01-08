8f9c8d39977c1c9116d4956a9d02158deb1636f9
bastiaanv <bastiankpn7800@gmail.com>
2026-01-08 20:51:31 +0100

    feat: finalize E3 alarms

--- EversenseKit/Packets/Enums/Alarm.swift
+++ EversenseKit/Packets/Enums/Alarm.swift
@@ -72,6 +72,7 @@ public enum Alarm: UInt8, Codable {
     case TransmitterOrSensorConnectionLostDuringLinking = 65
     case PairingMetricsError = 66
     case SmfSyncFailed = 67
+    case OtaUpgradeComplete = 69
     case NoAlarmActive = 74
     case TwoCal = 90
     case unknown = 255
@@ -108,6 +109,7 @@ public enum Alarm: UInt8, Codable {
              .SensorRetiringSoon7Alarm,
              .SensorSyncConfirmedAlarm,
              .TwoCal,
+             .OtaUpgradeComplete,
              .WarmUpPhaseCompleteAlert:
             return .Info
 
@@ -249,6 +251,8 @@ public enum Alarm: UInt8, Codable {
         case .SmfSyncFailed:
             return LocalizedString("SMF Sync Failed", comment: "title for SmfSyncFailed")
         case .NoAlarmActive:
+            return LocalizedString("", comment: "title for NoAlarmActive")
+        case .OtaUpgradeComplete:
             return LocalizedString("", comment: "title for NoAlarmActive")
         case .unknown:
             return LocalizedString("Unknown error", comment: "title for unknown")
